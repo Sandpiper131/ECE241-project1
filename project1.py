02dfa49d24c150338fae18a89bbd6dcbe4586e93
Luke <luke.lacasse13@gmail.com>
2020-10-07 15:23:11 -0400

    added quicksort

--- project1.py
+++ project1.py
@@ -37,7 +37,10 @@ class COV19Library:
             citsta = str.split(line[cindex[0] + 1 : cindex[1]])  # splits city and state field by space into array of string
             stindex = len(citsta) - 1  # index of state name
             stateName = citsta[stindex]  # Store state name
-            citName = line[cindex[0] + 1: cindex[1] - (len(stateName) + 1)]  # Format City Name
+            if len(citsta) == 1:
+                citName = citsta[0]
+            else:
+                citName = line[cindex[0] + 1: cindex[1] - (len(stateName) + 1)]  # Format City Name
             dlen = len(line) - len(line[cindex[3] + 1: cindex[len(cindex) - 1]])  #how many characters are left after the last comma
             temp = City(line[0:cindex[0]], citName, stateName, line[cindex[2] + 1: cindex[3]], line[cindex[len(cindex) - 1] + 1 : cindex[len(cindex) - 1] + dlen]) #line[cindex[3] + 1: cindex[len(cindex) - 1] + dlen])
             self.cityArray.append(temp)
@@ -54,6 +57,46 @@ class COV19Library:
         return "City not found"
 
 
+    def quickSort(self):
+        self.quickSortHelper(0,len(self.cityArray) - 1)
+        self.isSorted = True
+
+    def quickSortHelper(self, first, last):
+        if first<last:
+            splitpoint = self.partition(first, last)
+
+            self.quickSortHelper(first, splitpoint - 1)
+            self.quickSortHelper(splitpoint + 1, last)
+
+    def partition(self, first, last):
+        pivotvalue = self.cityArray[first].cname
+
+        leftmark = first + 1
+        rightmark = last
+
+        done = False
+        while not done:
+
+            while leftmark <= rightmark and self.cityArray[leftmark].cname <= pivotvalue:
+                leftmark = leftmark + 1
+
+            while self.cityArray[rightmark].cname >= pivotvalue and rightmark >= leftmark:
+                rightmark = rightmark - 1
+
+            if rightmark < leftmark:
+                done = True
+            else:
+                temp = self.cityArray[leftmark]
+                self.cityArray[leftmark] = self.cityArray[rightmark]
+                self.cityArray[rightmark] = temp
+
+        temp = self.cityArray[first]
+        self.cityArray[first] = self.cityArray[rightmark]
+        self.cityArray[rightmark] = temp
+
+        return rightmark
+
+
 
 
 # Press the green button in the gutter to run the script.
@@ -63,3 +106,7 @@ if __name__ == '__main__':
     lib.linearSearch("23700","id")
     lib.linearSearch("Lafayette","name")
     lib.linearSearch("Beebo","name")
+    lib.quickSort()
+    for city in lib.cityArray:
+        print(city)
+    print(lib.isSorted)
