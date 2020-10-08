11ed377add212d9f8a6d17297b5e2fbb40f8a605
Luke <luke.lacasse13@gmail.com>
2020-10-08 17:56:49 -0400

    fixed bst balance

--- project1.py
+++ project1.py
@@ -3,6 +3,8 @@
 # Title: ECE 241 Project 1: Sorting and Searching
 # Description:
 
+#from BinarySearchTree import BinarySearchTree
+
 class City:
 
     def __init__(self, cid, cname, cstate, pop, cities):
@@ -22,6 +24,8 @@ class COV19Library:
         self.cityArray = []
         self.size = 0
         self.isSorted = False
+        self.BST = None
+        self.root = None
 
     def LoadData(self, filename):
         file = open(filename, "r")  # open csv file as read-only
@@ -56,13 +60,12 @@ class COV19Library:
                     return str(tempcity)
         return "City not found"
 
-
     def quickSort(self):
         self.quickSortHelper(0,len(self.cityArray) - 1)
         self.isSorted = True
 
     def quickSortHelper(self, first, last):
-        if first<last:
+        if first < last:
             splitpoint = self.partition(first, last)
 
             self.quickSortHelper(first, splitpoint - 1)
@@ -96,6 +99,177 @@ class COV19Library:
 
         return rightmark
 
+    def buildBST(self):
+        self.BST = BinarySearchTree()
+        for city in self.cityArray:
+            self.BST.put(int(city.cid), city)
+        self.root = self.BST.root
+
+    def searchBST(self, cid):
+        try:
+            temp = self.BST.get(int(cid))
+            if temp != None:
+                return temp
+            else:
+                return "City not found"
+        except:
+            return "City not found"
+
+
+class TreeNode:
+    def __init__(self, key, val, left=None, right=None, parent=None):
+        self.key = key
+        self.payload = val
+        self.leftChild = left
+        self.rightChild = right
+        self.parent = parent
+        self.balanceFactor = 0
+
+    def hasLeftChild(self):
+        return self.leftChild
+
+    def hasRightChild(self):
+        return self.rightChild
+
+    def isLeftChild(self):
+        return self.parent and self.parent.leftChild == self
+
+    def isRightChild(self):
+        return self.parent and self.parent.rightChild == self
+
+    def isRoot(self):
+        return not self.parent
+
+
+class BinarySearchTree:
+
+    def __init__(self):
+        self.root = None
+        self.size = 0
+
+    def length(self):
+        return self.size
+
+    def __len__(self):
+        return self.size
+
+    def put(self, key, val):
+        if self.root:
+            self._put(key, val, self.root)
+        else:
+            self.root = TreeNode(key, val)
+        self.size = self.size + 1
+
+    def _put(self,key, val, currentNode):
+        if key < currentNode.key:
+            if currentNode.hasLeftChild():
+                self._put(key, val, currentNode.leftChild)
+            else:
+                currentNode.leftChild = TreeNode(key, val, parent=currentNode)
+                self.updateBalance(currentNode.leftChild)
+        else:
+            if currentNode.hasRightChild():
+                self._put(key, val, currentNode.rightChild)
+            else:
+                currentNode.rightChild = TreeNode(key, val, parent=currentNode)
+                self.updateBalance(currentNode.rightChild)
+
+    def __setitem__(self, k, v):
+        self.put(k, v)
+
+    def updateBalance(self, node):
+        if node.balanceFactor > 1 or node.balanceFactor < -1:
+            self.rebalance(node)
+            return
+        if node.parent != None:
+            if node.isLeftChild():
+                node.parent.balanceFactor += 1
+            elif node.isRightChild():
+                node.parent.balanceFactor -= 1
+            if node.parent.balanceFactor != 0:
+                self.updateBalance(node.parent)
+
+    def rotateLeft(self, rotRoot):
+        newRoot = rotRoot.rightChild
+
+        rotRoot.rightChild = newRoot.leftChild
+        if newRoot.leftChild != None:
+            newRoot.lrftChild.parent = rotRoot
+        newRoot.parent = rotRoot.parent
+        if rotRoot.isRoot():
+            self.root = newRoot
+        else:
+            if rotRoot.isLeftChild():
+                rotRoot.parent.leftChild = newRoot
+            else:
+                rotRoot.parent.rightChild = newRoot
+        newRoot.leftChild = rotRoot
+        rotRoot.parent = newRoot
+        rotRoot.balanceFactor = rotRoot.balanceFactor - 1 - max(newRoot.balanceFactor, 0)
+        newRoot.balanceFactor = newRoot.balanceFactor - 1 + min(rotRoot.balanceFactor, 0)
+
+    def rotateRight(self, rotRoot):
+        newRoot = rotRoot.leftChild
+
+        rotRoot.leftChild = newRoot.rightChild
+        if newRoot.rightChild != None:
+            newRoot.rightChild.parent = rotRoot
+        newRoot.parent = rotRoot.parent
+        if rotRoot.isRoot():
+            self.root = newRoot
+        else:
+            if rotRoot.isRightChild():
+                rotRoot.parent.rightChild = newRoot
+            else:
+                rotRoot.parent.leftChild = newRoot
+        newRoot.rightChild = rotRoot
+        rotRoot.parent = newRoot
+        rotRoot.balanceFactor = rotRoot.balanceFactor - 1 - max(newRoot.balanceFactor, 0)
+        newRoot.balanceFactor = newRoot.balanceFactor - 1 + min(rotRoot.balanceFactor, 0)
+
+    def rebalance(self, node):
+        if node.balanceFactor < 0:
+            if node.rightChild.balanceFactor > 0:
+                self.rotateRight(node.rightChild)
+                self.rotateLeft(node)
+            else:
+                self.rotateLeft(node)
+        elif node.balanceFactor > 0:
+            if node.leftChild.balanceFactor < 0:
+                self.rotateLeft(node.leftChild)
+                self.rotateRight(node)
+            else:
+                self.rotateRight(node)
+
+    def get(self, key):
+        if self.root:
+            res = self._get(key, self.root)
+            if res:
+                return res.payload
+            else:
+                return None
+        else:
+            return None
+
+    def _get(self, key, currentNode):
+        if not currentNode:
+            return None
+        elif currentNode.key == key:
+            return currentNode
+        elif key < currentNode.key:
+            return self._get(key, currentNode.leftChild)
+        else:
+            return self._get(key, currentNode.rightChild)
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def __contains__(self, key):
+        if self._get(key, self.root):
+            return True
+        else:
+            return False
+
 
 
 
@@ -107,6 +281,5 @@ if __name__ == '__main__':
     lib.linearSearch("Lafayette","name")
     lib.linearSearch("Beebo","name")
     lib.quickSort()
-    for city in lib.cityArray:
-        print(city)
-    print(lib.isSorted)
+    lib.buildBST()
+    print(lib.searchBST("14780"))
